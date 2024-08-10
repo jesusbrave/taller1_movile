bee75aa2004115129dea0403c1efaec4642aa180
jesusbrave <jvaliente737@gmial.com>
2024-08-10 18:20:41 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'taller1_movile',
+  webDir: 'www'
+};
+
+export default config;
