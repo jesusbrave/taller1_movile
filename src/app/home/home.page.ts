bbaeebb4b1076da9c6201218a7c7663f1d6f2228
jesusbrave <jvaliente737@gmial.com>
2024-08-11 14:52:22 -0500

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  titulo: string = '';
+  descripcion: string = '';
+  tasks: { titulo: string; descripcion: string; done: boolean }[] = [];
 
   constructor() {}
 
+  onSubmit(form: NgForm) {
+    if (this.titulo && this.descripcion) {
+
+      const formattedTitulo = (this.titulo.trim());
+      const formattedDescripcion = (this.descripcion.trim());
+
+      this.tasks.push({ titulo: formattedTitulo, descripcion: formattedDescripcion, done: false });
+
+      console.log('Título (formateado):', formattedTitulo);
+      console.log('Descripción (formateada):', formattedDescripcion);
+
+      form.reset();
+    }
+  }
+
+
+  toggleDone(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
